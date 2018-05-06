0beb2534894895503f19140c31c61243adc77ac1
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-05-05 22:09:25 -0600

    securified

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,27 @@ var mysql = require("mysql");
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "Livinin1",
-	database: "burgers_db"
-});
+var connection;
+
+// var connection = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "Livinin1",
+// 	database: "burgers_db"
+// });
+
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			password: process.env.PASSWORD,
+			database: "burgers_db"
+		});
+}
 
 connection.connect(function (err) {
 	if (err) {
